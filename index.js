faed58f9405588c85f5830bea25742bdc580628d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-21 12:25:37 -0700

    Adding places controller

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
