408b89edf7fc3ab4e397291e6a629e1bbec9215b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-21 12:16:22 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
