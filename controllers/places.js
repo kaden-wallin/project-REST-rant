5a21545e1f5f8f1707647f0458b8f209c1cde40b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-23 21:38:06 -0700

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -21,4 +21,10 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
 module.exports = router
