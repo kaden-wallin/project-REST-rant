c6ec8745f3f48bd1e9746ac62d7c50a4adda4c4c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-27 21:15:52 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,21 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/doha-restaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/pink-cabana.jpg'
-      }]      
-    res.render('places/index', { places })
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
