51e56c68b87b31565baecb6eae0c293cb4843dee
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 20:28:08 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -16,10 +16,23 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
 
 router.post('/', (req, res) => {
   console.log(req.body)
